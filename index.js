6d7053fc9e309c27f24ef1c44dc0c5a1a970ba15
Cory Bass <cory.bass.07@gmail.com>
2021-05-26 15:47:40 -0500

    restructured files

--- index.js
+++ index.js
@@ -1,34 +1,9 @@
 require("dotenv").config();
 
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
+require("./services/passport");
 
 const app = express();
-
-passport.use(
-    new GoogleStrategy(
-        {
-            clientID: process.env.CLIENT_ID,
-            clientSecret: process.env.CLIENT_SECRET,
-            callbackURL: "/auth/google/callback",
-        },
-        (accessToken, refreshToken, profile, done) => {
-            console.log("access token", accessToken);
-            console.log("refresh token", refreshToken);
-            console.log("profile", profile);
-        }
-    )
-);
-
-app.get(
-    "/auth/google",
-    passport.authenticate("google", {
-        scope: ["profile", "email"],
-    })
-);
-
-app.get("/auth/google/callback", passport.authenticate("google"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
