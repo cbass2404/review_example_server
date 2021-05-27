848d67822250b222c4fe22237dd666c093be778e
Cory Bass <cory.bass.07@gmail.com>
2021-05-26 23:09:15 -0500

    added mongoose to files

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 require("./services/passport");
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+});
 
 const app = express();
 
