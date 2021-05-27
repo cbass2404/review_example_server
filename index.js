6ecaca633d38e805598a1a26ffa97d94241cec99
Cory Bass <cory.bass.07@gmail.com>
2021-05-27 00:04:02 -0500

    added in ability to add user to db

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-require("./services/passport");
 require("./models/User");
+require("./services/passport");
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
