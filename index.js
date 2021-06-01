77c6e903868c91b45076cfee2897ab26617efc2d
Cory Bass <cory.bass.07@gmail.com>
2021-06-01 16:08:40 -0500

    added catch all on server for routes not in server

--- index.js
+++ index.js
@@ -28,5 +28,18 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+    // express will serve up production assets
+    // main.js or main.css
+    app.use(express.static("client/build"));
+
+    // express wil serve up index.html
+    // if it doesn't recognize route
+    const path = require("path");
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
