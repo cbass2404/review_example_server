34ba6344677f923f657c1bbab30defaa059bf7a0
Cory Bass <cory.bass.07@gmail.com>
2021-06-07 16:02:24 -0500

    added surveyList component

---
+++ client/src/components/surveys/SurveyList.js
@@ -0,0 +1,39 @@
+import { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchSurveys } from "../../actions";
+
+const SurveyList = ({ surveys, fetchSurveys }) => {
+    console.log(surveys);
+
+    useEffect(() => {
+        fetchSurveys();
+    }, [fetchSurveys]);
+
+    const renderSurveys = () =>
+        surveys.map((survey) => {
+            return (
+                <div className="card darken-1" key={survey._id}>
+                    <div className="card-content">
+                        <span className="card-title">{survey.title}</span>
+                        <p>{survey.body}</p>
+                        <p className="right">
+                            Sent on:{" "}
+                            {new Date(survey.dateSent).toLocaleDateString()}
+                        </p>
+                        <div className="card-action">
+                            <a>Yes: {survey.yes}</a>
+                            <a>No: {survey.no}</a>
+                        </div>
+                    </div>
+                </div>
+            );
+        });
+
+    return <div>{renderSurveys()}</div>;
+};
+
+const mapStateToProps = ({ surveys }) => {
+    return { surveys };
+};
+
+export default connect(mapStateToProps, { fetchSurveys })(SurveyList);
